9c2de57ef01f37ae6b278b21328c3ba9b0e6dfac
Patrick Anjos <patrickpqdt87289@gmail.com>
2023-10-06 14:31:26 -0300

    initial commit

---
+++ gerency/src/main/java/com/setxpro/gerency/GerencyApplication.java
@@ -0,0 +1,13 @@
+package com.setxpro.gerency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerencyApplication.class, args);
+	}
+
+}
---
+++ gerency/src/test/java/com/setxpro/gerency/GerencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setxpro.gerency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
